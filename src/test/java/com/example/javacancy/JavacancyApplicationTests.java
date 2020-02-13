58657da251a148d2de49a5b724a7adb527057237
Halvard Bastiansen <Halvard.bastiansen@gmail.com>
2020-02-13 13:52:09 +0100

    initial commit

---
+++ src/test/java/com/example/javacancy/JavacancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacancyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
