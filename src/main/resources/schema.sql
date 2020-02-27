c1b894f6b2770bd9b9076c67f64ae1f7bca6c250
marianne-ott <mar.ottersen@gmail.com>
2020-02-27 14:47:33 +0100

    syntax errors fixed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,24 @@
 CREATE TABLE VACANCY(
-  ID BIGINT AUTO_INCREMENT PRIMARY KEY,
-  jobId VARCHAR (8), NOT NULL,
-  jobTitle VARCHAR(40) NOT NULL,
-  companyName VARCHAR(40) NOT NULL,
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  job_id VARCHAR (8) NOT NULL,
+  job_title VARCHAR(40) NOT NULL,
+  company_name VARCHAR(40) NOT NULL,
   location ENUM ('Oslo', 'Trondheim', 'Bergen', 'Stavanger') NOT NULL,
   experience ENUM ('Entry', 'Mid', 'Senior') NOT NULL,
   salary ENUM ('Level1', 'Level2', 'Level3', 'Level4') NOT NULL,
-  jobDescription VARCHAR(5000) NOT NULL,
-  searchRelevance BIGINT
+  job_description VARCHAR(5000) NOT NULL,
+  search_relevance BIGINT
 );
 
 
 CREATE TABLE APPLICATION (
-ID BIGINT AUTO_INCREMENT PRIMARY KEY,
-firstName VARCHAR (25),
-lastName VARCHAR (25),
+id BIGINT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR (25),
+last_name VARCHAR (25),
 email VARCHAR (40),
-phoneNumber BIGINT,
-applicationText VARCHAR (5000),
-vacancyId BIGING FOREIGN KEY
+phone_number BIGINT,
+application_text VARCHAR (5000),
+vacancy_id BIGINT FOREIGN KEY (vacancy_id) REFERENCES VACANCY(id)
 );
 
 
